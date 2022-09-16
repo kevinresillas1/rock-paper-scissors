72b3be3e86b7018d786362270e88dcfc5369d163
kevinresillas1@gmail.com <kevinresillas1@gmail.com>
2022-09-16 16:38:13 -0700

    sample

---
+++ main.js
@@ -0,0 +1,54 @@
+const selectionBtns = document.querySelectorAll("[data-selection]");
+const finalColumn = document.querySelector("[data-final-column]");
+const SELECTIONS = [
+  {
+    name: "rock",
+    emoji: "✊",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    emoji: "🤚",
+    beats: "rock",
+  },
+  {
+    name: "scissors",
+    emoji: "✌️",
+    beats: "paper",
+  },
+];
+
+selectionBtns.forEach((selectionBtn) => {
+  selectionBtn.addEventListener("click", (e) => {
+    const selectionName = selectionBtn.dataset.selection;
+    const selection = SELECTIONS.find(
+      (selection) => selection.name === selectionName
+    );
+    makeSelection(selection);
+  });
+});
+
+function makeSelection(selection) {
+  const computerSelection = randomSelection();
+  const yourWinner = isWinner(selection, computerSelection);
+  const computerWinner = isWinner(computerSelection, selection);
+  addSelectionResult(computerSelection, computerWinner);
+  addSelectionResult(selection, yourWinner);
+}
+
+function randomSelection() {
+  const randomIndex = Math.floor(Math.random() * SELECTIONS.length);
+  return SELECTIONS[randomIndex];
+}
+
+function isWinner(selection, opponentSelection) {
+  return selection.beats === opponentSelection.name;
+}
+
+function addSelectionResult(selection, winner) {
+  const div = document.createElement("div");
+  div.innerText = selection.emoji;
+  div.classList.add("results-selection");
+  if (winner) div.classList.add("winner");
+  finalColumn.after(div);
+}
